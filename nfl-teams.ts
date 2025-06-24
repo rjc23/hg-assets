31c48addea1eff6eeba248c3c29270725ab1d16f
Ryan <ryancarmody@outlook.com>
2025-06-24 15:33:05 +0930

    Create nfl-teams.ts

---
+++ nfl-teams.ts
@@ -0,0 +1,295 @@
+export const teams = [
+  "ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN",
+  "DET", "GNB", "HOU", "IND", "JAX", "KAN", "LAC", "LAR", "LVR", "MIA",
+  "MIN", "NWE", "NOR", "NYG", "NYJ", "PHI", "PIT", "SEA", "SFO", "TAM",
+  "TEN", "WAS"
+];
+
+export const teamsHistory = [
+  {
+    "mainCode": "ARI",
+    "teamName": "Arizona Cardinals",
+    "history": [
+      { "code": "ARI", "name": "Arizona Cardinals", "start": 1994, "end": 2024 },
+      { "code": "STL", "name": "St. Louis Cardinals", "start": 1960, "end": 1987 },
+      { "code": "CRD", "name": "Chicago Cardinals", "start": 1920, "end": 1959 },
+      { "code": "PHO", "name": "Phoenix Cardinals", "start": 1988, "end": 1993 }
+    ]
+  },
+  {
+    "mainCode": "ATL",
+    "teamName": "Atlanta Falcons",
+    "history": [
+      { "code": "ATL", "name": "Atlanta Falcons", "start": 1966, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "BAL",
+    "teamName": "Baltimore Ravens",
+    "history": [
+      { "code": "BAL", "name": "Baltimore Ravens", "start": 1996, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "BUF",
+    "teamName": "Buffalo Bills",
+    "history": [
+      { "code": "BUF", "name": "Buffalo Bills", "start": 1960, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "CAR",
+    "teamName": "Carolina Panthers",
+    "history": [
+      { "code": "CAR", "name": "Carolina Panthers", "start": 1995, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "CHI",
+    "teamName": "Chicago Bears",
+    "history": [
+      { "code": "CHI", "name": "Chicago Bears", "start": 1920, "end": 2024 },
+      { "code": "CHI", "name": "Chicago Staleys", "start": 1921, "end": 1921 },
+      { "code": "CHI", "name": "Decatur Staleys", "start": 1920, "end": 1920 }
+    ]
+  },
+  {
+    "mainCode": "CIN",
+    "teamName": "Cincinnati Bengals",
+    "history": [
+      { "code": "CIN", "name": "Cincinnati Bengals", "start": 1968, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "CLE",
+    "teamName": "Cleveland Browns",
+    "history": [
+      { "code": "CLE", "name": "Cleveland Browns", "start": 1946, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "DAL",
+    "teamName": "Dallas Cowboys",
+    "history": [
+      { "code": "DAL", "name": "Dallas Cowboys", "start": 1960, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "DEN",
+    "teamName": "Denver Broncos",
+    "history": [
+      { "code": "DEN", "name": "Denver Broncos", "start": 1960, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "DET",
+    "teamName": "Detroit Lions",
+    "history": [
+      { "code": "DET", "name": "Detroit Lions", "start": 1926, "end": 2024 },
+      { "code": "PRT", "name": "Portsmouth Spartans", "start": 1930, "end": 1933 }
+    ]
+  },
+  {
+    "mainCode": "GNB",
+    "teamName": "Green Bay Packers",
+    "history": [
+      { "code": "GNB", "name": "Green Bay Packers", "start": 1919, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "HOU",
+    "teamName": "Houston Texans",
+    "history": [
+      { "code": "HOU", "name": "Houston Texans", "start": 2002, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "IND",
+    "teamName": "Indianapolis Colts",
+    "history": [
+      { "code": "IND", "name": "Indianapolis Colts", "start": 1984, "end": 2024 },
+      { "code": "BAL", "name": "Baltimore Colts", "start": 1953, "end": 1983 }
+    ]
+  },
+  {
+    "mainCode": "JAX",
+    "teamName": "Jacksonville Jaguars",
+    "history": [
+      { "code": "JAX", "name": "Jacksonville Jaguars", "start": 1995, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "KAN",
+    "teamName": "Kansas City Chiefs",
+    "history": [
+      { "code": "KAN", "name": "Kansas City Chiefs", "start": 1924, "end": 2024 },
+      { "code": "DTX", "name": "Dallas Texans", "start": 1960, "end": 1962 }
+    ]
+  },
+  {
+    "mainCode": "LAC",
+    "teamName": "Los Angeles Chargers",
+    "history": [
+      { "code": "LAC", "name": "Los Angeles Chargers", "start": 1960, "end": 1960 },
+      { "code": "SDG", "name": "San Diego Chargers", "start": 1961, "end": 2016 },
+      { "code": "LAC", "name": "Los Angeles Chargers", "start": 2017, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "LAR",
+    "teamName": "Los Angeles Rams",
+    "history": [
+      { "code": "LAR", "name": "Los Angeles Rams", "start": 2016, "end": 2024 },
+      { "code": "RAM", "name": "Los Angeles Rams", "start": 1946, "end": 1994 },
+      { "code": "STL", "name": "St. Louis Rams", "start": 1995, "end": 2015 },
+      { "code": "RAM", "name": "Cleveland Rams", "start": 1936, "end": 1945 }
+    ]
+  },
+  {
+    "mainCode": "LVR",
+    "teamName": "Las Vegas Raiders",
+    "history": [
+      { "code": "LVR", "name": "Las Vegas Raiders", "start": 2020, "end": 2024 },
+      { "code": "OAK", "name": "Oakland Raiders", "start": 1995, "end": 2019 },
+      { "code": "RAI", "name": "Los Angeles Raiders", "start": 1982, "end": 1994 },
+      { "code": "OAK", "name": "Oakland Raiders", "start": 1960, "end": 1981 }
+    ]
+  },
+  {
+    "mainCode": "MIA",
+    "teamName": "Miami Dolphins",
+    "history": [
+      { "code": "MIA", "name": "Miami Dolphins", "start": 1966, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "MIN",
+    "teamName": "Minnesota Vikings",
+    "history": [
+      { "code": "MIN", "name": "Minnesota Vikings", "start": 1961, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "NWE",
+    "teamName": "New England Patriots",
+    "history": [
+      { "code": "NWE", "name": "New England Patriots", "start": 1971, "end": 2024 },
+      { "code": "BOS", "name": "Boston Patriots", "start": 1960, "end": 1970 }
+    ]
+  },
+  {
+    "mainCode": "NOR",
+    "teamName": "New Orleans Saints",
+    "history": [
+      { "code": "NOR", "name": "New Orleans Saints", "start": 1967, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "NYG",
+    "teamName": "New York Giants",
+    "history": [
+      { "code": "NYG", "name": "New York Giants", "start": 1925, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "NYJ",
+    "teamName": "New York Jets",
+    "history": [
+      { "code": "NYJ", "name": "New York Jets", "start": 1963, "end": 2024 },
+      { "code": "NYT", "name": "New York Titans", "start": 1960, "end": 1962 }
+    ]
+  },
+  {
+    "mainCode": "PHI",
+    "teamName": "Philadelphia Eagles",
+    "history": [
+      { "code": "PHI", "name": "Philadelphia Eagles", "start": 1933, "end": 2024 },
+      { "code": "PHI", "name": "Pittsburgh/Philadelphia Eagles (Steagles)", "start": 1943, "end": 1943 }
+    ]
+  },
+  {
+    "mainCode": "PIT",
+    "teamName": "Pittsburgh Steelers",
+    "history": [
+      { "code": "PIT", "name": "Pittsburgh Steelers", "start": 1940, "end": 2024 },
+      { "code": "PHI", "name": "Pittsburgh/Philadelphia Eagles (Steagles)", "start": 1943, "end": 1943 },
+      { "code": "PIT", "name": "Pittsburgh Pirates", "start": 1933, "end": 1939 }
+    ]
+  },
+  {
+    "mainCode": "SFO",
+    "teamName": "San Francisco 49ers",
+    "history": [
+      { "code": "SFO", "name": "San Francisco 49ers", "start": 1946, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "SEA",
+    "teamName": "Seattle Seahawks",
+    "history": [
+      { "code": "SEA", "name": "Seattle Seahawks", "start": 1976, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "TAM",
+    "teamName": "Tampa Bay Buccaneers",
+    "history": [
+      { "code": "TAM", "name": "Tampa Bay Buccaneers", "start": 1976, "end": 2024 }
+    ]
+  },
+  {
+    "mainCode": "TEN",
+    "teamName": "Tennessee Titans",
+    "history": [
+      { "code": "TEN", "name": "Tennessee Titans", "start": 1999, "end": 2024 },
+      { "code": "TEN", "name": "Tennessee Oilers", "start": 1997, "end": 1998 },
+      { "code": "HOU", "name": "Houston Oilers", "start": 1960, "end": 1996 }
+    ]
+  },
+  {
+    "mainCode": "WAS",
+    "teamName": "Washington Commanders",
+    "history": [
+      { "code": "WAS", "name": "Washington Commanders", "start": 2022, "end": 2024 },
+      { "code": "WAS", "name": "Washington Football Team", "start": 2020, "end": 2021 },
+      { "code": "WAS", "name": "Washington Redskins", "start": 1937, "end": 2019 },
+      { "code": "BOS", "name": "Boston Redskins", "start": 1933, "end": 1936 },
+      { "code": "BOS", "name": "Boston Braves", "start": 1932, "end": 1932 }
+    ]
+  }
+]
+
+export const teamMap = new Map([
+  ['ARI', 'Arizona Cardinals'],
+  ['ATL', 'Atlanta Falcons'],
+  ['BAL', 'Baltimore Ravens'],
+  ['BUF', 'Buffalo Bills'],
+  ['CAR', 'Carolina Panthers'],
+  ['CHI', 'Chicago Bears'],
+  ['CIN', 'Cincinnati Bengals'],
+  ['CLE', 'Cleveland Browns'],
+  ['DAL', 'Dallas Cowboys'],
+  ['DEN', 'Denver Broncos'],
+  ['DET', 'Detroit Lions'],
+  ['GNB', 'Green Bay Packers'],
+  ['HOU', 'Houston Texans'],
+  ['IND', 'Indianapolis Colts'],  
+  ['JAX', 'Jacksonville Jaguars'],
+  ['KAN', 'Kansas City Chiefs'],
+  ['LAC', 'Los Angeles Chargers'],
+  ['LAR', 'Los Angeles Rams'],
+  ['LVR', 'Las Vegas Raiders'],
+  ['MIA', 'Miami Dolphins'],
+  ['MIN', 'Minnesota Vikings'],
+  ['NWE', 'New England Patriots'],
+  ['NOR', 'New Orleans Saints'],
+  ['NYG', 'New York Giants'],
+  ['NYJ', 'New York Jets'],
+  ['PHI', 'Philadelphia Eagles'],
+  ['PIT', 'Pittsburgh Steelers'],
+  ['SEA', 'Seattle Seahawks'],
+  ['SFO', 'San Francisco 49ers'],
+  ['TAM', 'Tampa Bay Buccaneers'],
+  ['TEN', 'Tennessee Titans'],
+  ['WAS', 'Washington Commanders']
+]);
