3da1a163628b1f4f942117432903b83a81d563ff
Ryan <ryancarmody@outlook.com>
2025-06-24 15:34:02 +0930

    Create nba-teams.ts

---
+++ nba-teams.ts
@@ -0,0 +1,35 @@
+export const teams = ["BOS", "ATL", "BKN", "CHA", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", "OKC", "ORL", "PHI", "PHX", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"];
+
+export const teamMap = new Map([
+  ['BOS', 'Boston Celtics'],
+  ['ATL', 'Atlanta Hawks'],
+  ['BKN', 'Brooklyn Nets'],
+  ['CHA', 'Charlotte Hornets'],
+  ['CHI', 'Chicago Bulls'],
+  ['CLE', 'Cleveland Cavaliers'],
+  ['DAL', 'Dallas Mavericks'],
+  ['DEN', 'Denver Nuggets'],
+  ['DET', 'Detroit Pistons'],
+  ['GSW', 'Golden State Warriors'],
+  ['HOU', 'Houston Rockets'],
+  ['IND', 'Indiana Pacers'],
+  ['LAC', 'Los Angeles Clippers'],
+  ['LAL', 'Los Angeles Lakers'],
+  ['MEM', 'Memphis Grizzlies'],
+  ['MIA', 'Miami Heat'],
+  ['MIL', 'Milwaukee Bucks'],
+  ['MIN', 'Minnesota Timberwolves'],
+  ['NOP', 'New Orleans Pelicans'],
+  ['NYK', 'New York Knicks'],
+  ['OKC', 'Oklahoma City Thunder'],
+  ['ORL', 'Orlando Magic'],
+  ['PHI', 'Philadelphia 76ers'],
+  ['PHX', 'Phoenix Suns'],
+  ['POR', 'Portland Trail Blazers'],
+  ['SAC', 'Sacramento Kings'],
+  ['SAS', 'San Antonio Spurs'],
+  ['TOR', 'Toronto Raptors'],
+  ['UTA', 'Utah Jazz'],
+  ['WAS', 'Washington Wizards']
+]);
+
